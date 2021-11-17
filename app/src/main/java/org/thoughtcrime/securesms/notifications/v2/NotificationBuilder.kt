19cd1e9db3fe3c10ff2d6a464f259fc9862403e1
Laurent Sauvage <sauvage.laurent@gmail.com>
2021-11-17 22:02:13 +0100

    Android 12 support

--- app/src/main/java/org/thoughtcrime/securesms/notifications/v2/NotificationBuilder.kt
+++ app/src/main/java/org/thoughtcrime/securesms/notifications/v2/NotificationBuilder.kt
@@ -335,7 +335,7 @@ sealed class NotificationBuilder(protected val context: Context) {
         context,
         0,
         ConversationIntents.createBubbleIntent(context, conversation.recipient.id, conversation.threadId),
-        0
+        PendingIntent.FLAG_IMMUTABLE
       )
 
       val bubbleMetadata = NotificationCompat.BubbleMetadata.Builder(intent, AvatarUtil.getIconCompatForShortcut(context, conversation.recipient))
